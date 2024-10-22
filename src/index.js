302660b30e7af70681ad76abd4c5cc559d368281
mrynp <110476086+mrynp@users.noreply.github.com>
2024-10-22 23:33:56 +0800

    feat: add basic footer structure and content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import loadHome from "./home";
+import character from "./character.png";
 
 function createHeader() {
   const header = document.createElement("header");
@@ -32,14 +33,105 @@ function createHeader() {
   return header;
 }
 
+function createFooter() {
+  const footer = document.createElement("footer");
+
+  const characterContainer = document.createElement("div");
+  characterContainer.classList.add("characterContainer");
+
+  const characterImg = document.createElement("img");
+  characterImg.src = character;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("text-container");
+
+  const detailsContainer = document.createElement("div");
+  detailsContainer.classList.add("details-container");
+
+  const details1 = document.createElement("div");
+  details1.classList.add("details");
+  details1.appendChild(createH3("Contact us"));
+  details1.appendChild(
+    createP(
+      "To get in touch, visit our contact page or call 88888888. For employment opportunities, see our current vacancies."
+    )
+  );
+
+  const details2 = document.createElement("div");
+  details2.classList.add("details");
+  details2.appendChild(createH3("Opening hours"));
+  details2.appendChild(createP("Tuesday - Thursday 4.30pm - 11pm"));
+  details2.appendChild(createP("Friday & Saturday 12pm till late"));
+
+  const details3 = document.createElement("div");
+  details3.classList.add("details");
+  details3.appendChild(createP("Instagram"));
+  details3.appendChild(createP("Facebook"));
+
+  detailsContainer.appendChild(details1);
+  detailsContainer.appendChild(details2);
+
+  const creditContainer = document.createElement("div");
+  creditContainer.classList.add("credit-container");
+
+  creditContainer.appendChild(createH3("@mrynp"));
+
+  textContainer.appendChild(detailsContainer);
+  textContainer.appendChild(creditContainer);
+  characterContainer.appendChild(characterImg);
+
+  footer.appendChild(characterContainer);
+  footer.appendChild(textContainer);
+
+  return footer;
+}
+
 function initializeWebsite() {
   const content = document.getElementById("content");
   const header = createHeader();
 
   document.body.appendChild(header);
   document.body.appendChild(content);
+  document.body.appendChild(createFooter());
 
   loadHome();
+}
+
+function createH1(text) {
+  const h1 = document.createElement("h1");
+  h1.textContent = text;
+  return h1;
+}
+
+function createH2(text) {
+  const h2 = document.createElement("h2");
+  h2.textContent = text;
+  return h2;
+}
+
+function createH3(text) {
+  const h3 = document.createElement("h3");
+  h3.textContent = text;
+  return h3;
+}
+
+function createH4(text) {
+  const h4 = document.createElement("h4");
+  h4.textContent = text;
+  return h4;
+}
+
+function createP(text) {
+  const p = document.createElement("p");
+  p.textContent = text;
+  return p;
+}
+
+function createButton(text) {
+  const button = document.createElement("button");
+  button.appendChild(createH3(text));
+  button.classList.add("button");
+  return button;
 }
 
 initializeWebsite();
