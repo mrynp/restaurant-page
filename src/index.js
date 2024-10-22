19b49246c7bf24b09ec3575b54ceadb8b6c7d71d
mrynp <110476086+mrynp@users.noreply.github.com>
2024-10-22 23:02:33 +0800

    Refactor: Merge website.js into index.js for simplicity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import initializeWebsite from "./website";
+import "./styles.css";
+import loadHome from "./home";
+
+function createHeader() {
+  const header = document.createElement("header");
+
+  const restaurantName = document.createElement("div");
+  restaurantName.classList.add("restaurant-name");
+  restaurantName.textContent = "The Odin Restau";
+
+  const nav = document.createElement("nav");
+  const homeButton = document.createElement("button");
+  const menuButton = document.createElement("button");
+  const bookButton = document.createElement("button");
+
+  homeButton.textContent = "Home";
+  menuButton.textContent = "Menu";
+  bookButton.textContent = "Book a table";
+
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    loadHome();
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(bookButton);
+
+  header.appendChild(restaurantName);
+  header.appendChild(nav);
+
+  return header;
+}
+
+function initializeWebsite() {
+  const content = document.getElementById("content");
+  const header = createHeader();
+
+  document.body.appendChild(header);
+  document.body.appendChild(content);
+
+  loadHome();
+}
 
 initializeWebsite();
